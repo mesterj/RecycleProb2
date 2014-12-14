41b17e3e8a2def0bcc43d2c4811165cdf0d79740
mesterj <mesterj@freemail.hu>
2014-12-14 20:47:18 +0100

    Rejected commit: Default

--- app/src/main/java/proba/joco/hu/recycleprob2/MyAdapter.java
+++ app/src/main/java/proba/joco/hu/recycleprob2/MyAdapter.java
@@ -28,20 +28,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         }
     }
 
-
-
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    /* you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView mTextView;
-        public ViewHolder(TextView v) {
-            super(v);
-            mTextView = v;
-        }
-    }*/
-
     // Provide a suitable constructor (depends on the kind of dataset)
     public MyAdapter(String[] myDataset) {
         mDataset = myDataset;
@@ -59,20 +45,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         ViewHolder viewHolder = new ViewHolder(itemLayoutView);
         return viewHolder;
     }
-
-
-    /* Create new views (invoked by the layout manager)
-    @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
-        // create a new view
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.my_text_view, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
-    }*/
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
